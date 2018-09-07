5ea46da1dee8f330ee41972c209f59186aecd302
leandro <lcarvalho.dev@gmail.com>
2018-09-07 20:39:16 -0300

    For.cpp

---
+++ SimpleCodes/For.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    
+    for(int i = 0; i <= 100; i++)
+    {
+        cout << i++ << endl;
+    }
+    
+    
+    getchar();
+    return 0;
+}
