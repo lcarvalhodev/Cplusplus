582ba5a9cf39297d698c35ad7329860870f37aa0
leandro <lcarvalho.dev@gmail.com>
2018-09-08 11:48:46 -0300

    ReverseString.cpp

---
+++ SimpleCodes/ReverseString.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+
+
+using namespace std;
+
+void reverseString( char name[] ){
+
+    int size;
+    
+    for(size = 0; name[size] ; size++);
+    for (int i = size - 1 ; i >= 0 ; i--)
+    {
+        std::cout << name[i] << '\n';
+    }
+    
+    
+
+}
+
+int main(int argc, char const *argv[])
+{
+
+    char name[] = "Name";
+
+    reverseString(name);
+
+    getchar();
+    return 0;
+}
